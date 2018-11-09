b851bc219d00f1f7806027d09ddfeb3fc6c0866b
Husnain <husnainali@novatoresols.com>
2018-11-09 11:22:50 +0500

    Changes in wishKart.pk - basic pod installed

---
+++ wishkart.pk/Pods/Target Support Files/Pods-wishkart.pk/Pods-wishkart.pk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wishkart_pk : NSObject
+@end
+@implementation PodsDummy_Pods_wishkart_pk
+@end
---
+++ wishkart.pk/Pods/Target Support Files/Pods-wishkart.pkTests/Pods-wishkart.pkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wishkart_pkTests : NSObject
+@end
+@implementation PodsDummy_Pods_wishkart_pkTests
+@end
